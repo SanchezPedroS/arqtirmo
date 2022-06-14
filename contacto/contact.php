15b723642ba8c3a2d5434ec78dc9d5e362dadf70
SanchezPedroS <106620491+SanchezPedroS@users.noreply.github.com>
2022-06-14 11:00:49 -0300

    a verrrr

---
+++ contacto/contact.php
@@ -0,0 +1,25 @@
+<?php
+
+    //llamando a los campos
+
+    $nombre = $_POST ['name'];
+    $email = $_POST ['email'];
+    $phone = $_POST ['phone'];
+    $message = $_POST ['message'];
+
+    //DATOS PARA EL CORREO
+
+    $destinatario = "pedro.ssanchez@hotmail.com";
+    $asunto = "Contacto desde la web";
+    
+    $contenido = "De: $nombre \n";
+    $contenido .= "Correo: $email \n";
+    $contenido .= "Telefono: $phone \n";
+    $contenido .= "$message";
+
+    //ENVIANDO MAIL
+
+    mail($destinatario, $asunto, $contenido);
+    header('Location: /contacto/msjEnviado.html');
+
+?>
